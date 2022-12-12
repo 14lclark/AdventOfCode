ef5ece3d0d752ce923dfba8d9c0536c96f1f58e5
Logan Clark <loganclark.1996@gmail.com>
2022-12-12 14:53:57 -0600

    complete day1 and add day 2

---
+++ 2022/day1.py
@@ -0,0 +1,40 @@
+# snack calories
+
+def part1():
+    large = 0
+    with open('day1.txt') as f:
+        acc = 0
+        for line in f:
+
+            if line == "\n":
+                if acc > large:
+                    large = acc
+                acc = 0
+                continue
+
+            acc += int(line)
+
+        if acc > large:
+            large = acc
+
+    print(large)
+
+def part2():
+    large = []
+    with open('day1.txt') as f:
+        acc = 0
+        for line in f:
+
+            if line == "\n":
+                large.append(acc)
+                acc = 0
+                continue
+
+            acc += int(line)
+
+        large.append(acc)
+        large.sort(reverse=True)
+
+    print(large[0]+large[1]+large[2])
+
+part2()
