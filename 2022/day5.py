f16b659fb169128afb20551d57b37da013a30de5
Logan Clark <loganclark.1996@gmail.com>
2022-12-14 00:23:01 -0600

    fix broken part1 (move_stack not using all params)

--- 2022/day5.py
+++ 2022/day5.py
@@ -9,14 +9,14 @@ def move_stack(stacks, moves, how_many_at_once=1):
     origin = moves[1] - 1
     dest = moves[2] - 1
     temp = []
-    for _ in range(num):
-        temp.append(final[origin].pop())
-    
-    for _ in range(num):
-        final[dest].append(temp.pop())
+    for _ in range(num // how_many_at_once):
+        for _ in range(how_many_at_once):
+            temp.append(final[origin].pop())
+        for _ in range(how_many_at_once):
+            final[dest].append(temp.pop())
     return final
 
-def part1():
+def part(number):
     with open('2022/day5.txt') as f:
         line = f.readline()
         
@@ -31,46 +31,21 @@ def part1():
         for i in stacks:
             while i[-1] == ' ':
                 i.pop()
-        for i in stacks:
-            print(i)
-        
-        print(line)
+    
         f.readline()
         for line in f:
             moves = [int(i) for i in line.rstrip().split() if 'o' not in i]
-            stacks = move_stack(stacks, moves)
-
+            if number == 1:
+                stacks = move_stack(stacks, moves, 1)
+            if number == 2:
+                stacks = move_stack(stacks, moves, moves[0])
         
         for i in stacks:
-            print(i.pop())
+            print(i.pop(), end="")
+            
+        print()
             
         
-def part2():
-        with open('2022/day5.txt') as f:
-            line = f.readline()
-            
-            stacks = []
-            while " 1" not in line:
-                stacks.append([*line.rstrip()])
-                line = f.readline()
-            
-            stacks = transpose(stacks[::-1])
-            stacks = [i for i in stacks if ('[' not in i) and (']' not in i)]
-            stacks = [stacks[i] for i in range(len(stacks)) if i%2==0]
-            for i in stacks:
-                while i[-1] == ' ':
-                    i.pop()
-            for i in stacks:
-                print(i)
-            
-            print(line)
-            f.readline()
-            for line in f:
-                moves = [int(i) for i in line.rstrip().split() if 'o' not in i]
-                stacks = move_stack(stacks, moves, moves[0])
 
-            
-            for i in stacks:
-                print(i.pop())
 
-part2()
+part(2)
