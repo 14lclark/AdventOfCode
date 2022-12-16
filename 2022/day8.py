3adf4c6a39cd792ee7c17963899bca40862dcaf9
Logan Clark <loganclark.1996@gmail.com>
2022-12-15 19:04:57 -0600

    finish day 8, want to look back at the is_visible sometime

--- 2022/day8.py
+++ 2022/day8.py
@@ -3,11 +3,13 @@
 def get_column(grid, col_num):
     return [grid[i][col_num] for i in range(len(grid))]
 
-def printurn(x):
+def print_return(x):
     print(x)
     return x
 
 def is_visible(grid, row_num, col_num) -> bool:
+    # doesn't work, not sure why
+    
     #row index is which row you are in
     #column index is which column you're in
     #  XXXXX
@@ -45,20 +47,18 @@ def is_visible(grid, row_num, col_num) -> bool:
     return not blocked
     
 
-def get_grid(txt: str) -> list[list[int]]:
+def get_grid(txt: str) -> "list[list[int]]":
     grid = []
     with open(txt) as f:
         for line in f:
             grid.append([*(line.rstrip())])
-
-    print(grid[0])
     return [[int(i) for i in row] for row in grid]
 
 
 
 def part1(txt):
     grid = get_grid(txt)
-    visible_num = len(printurn(visible_list(grid)))
+    visible_num = len(visible_list(grid))
     
     return(visible_num)
 
@@ -72,7 +72,6 @@ def left_visible(grid):
             if grid[row][col] > highest:
                 visible.append((row,col))
                 highest = grid[row][col]
-    # print(visible)    
     return visible
 
 def right_visible(grid):
@@ -85,7 +84,6 @@ def right_visible(grid):
             if grid[row][col] > highest:
                 visible.append((row,col))
                 highest = grid[row][col]
-    # print(visible)
     return visible
 
 def top_visible(grid):
@@ -98,10 +96,9 @@ def top_visible(grid):
             if grid[row][col] > highest:
                 visible.append((row,col))
                 highest = grid[row][col]
-    # print(visible)
     return visible
 
-def bottom_visible(grid: list[list[int]]) -> list:
+def bottom_visible(grid: "list[list[int]]") -> list:
     visible = []
     width = len(grid[0])
     height = len(grid)
@@ -111,7 +108,6 @@ def bottom_visible(grid: list[list[int]]) -> list:
             if grid[row][col] > highest:
                 visible.append((row,col))
                 highest = grid[row][col]
-    # print(visible)
     return visible
 
 def visible_list(grid):
@@ -119,6 +115,62 @@ def visible_list(grid):
              | set(right_visible(grid))) \
              | set(top_visible(grid))) \
              | set(bottom_visible(grid)))
-            
+    
+def scenic_scores(grid: "list[list[int]]", row_num, col_num):
+    col = get_column(grid, col_num)
+    row = grid[row_num]
+    if row_num == 0 or col_num == 0 \
+       or row_num == (len(col)-1) or col_num == (len(row)-1):
+        return 0
+    found_left = False
+    found_right = False
+    found_up = False
+    found_down = False
+    distance = 0
+    score = 1
+    self_height = grid[row_num][col_num]
+    
+    while True:
+        distance += 1
 
-print(part1('2022/day8.txt'))
+        if not found_left and \
+            (col_num - distance == 0 or self_height <= row[col_num-distance]):
+                found_left = True
+                score *= distance
+        if not found_right and \
+            (col_num + distance == len(row) - 1 or self_height <= row[col_num+distance]):
+                found_right = True
+                score *= distance
+
+        if not found_up and \
+            (row_num - distance == 0 or self_height <= col[row_num-distance]):
+                found_up = True
+                score *= distance
+
+        if not found_down and \
+            (row_num + distance == len(col) - 1 or self_height <= col[row_num+distance]):
+                found_down = True
+                score *= distance
+
+        if found_down and found_left and found_right and found_up:
+            break
+    
+    return score
+        
+def part2(txt):
+    grid = get_grid(txt)
+    grid_height = len(grid)
+    grid_width = len(grid[0])
+    scenic_score_max = 0
+    for row_num in range(grid_height):
+        for col_num in range(grid_width):
+            scenic_score = scenic_scores(grid, row_num, col_num)
+            if  scenic_score > scenic_score_max:
+                scenic_score_max = scenic_score
+    
+    return scenic_score_max
+    
+    
+    
+# print(part1('2022/day8.txt'))
+print(part2('2022/day8.txt'))
